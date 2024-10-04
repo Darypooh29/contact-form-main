8e60cb0cb7e309a37fc26617da696bfcaefb30fe
Dario Chavira <dchavira290@gmail.com>
2024-10-04 16:38:14 -0600

    Just missing to manage the functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const input_name = document.querySelector('#name');
+const lastname_input = document.querySelector('#lastname');
+const email_input = document.querySelector('#email');
+const radio_button_container = document.querySelectorAll('.field-container__query-type-container');
+
+radio_button_container.forEach(container => {
+  container.addEventListener('click', () => {
+    const radioButton = container.querySelector('input[type="radio"]');
+    
+    radioButton.checked = true;
+
+    const selectedValue = radioButton.value;
+    console.log('Selected Value:', selectedValue);
+  });
+});
