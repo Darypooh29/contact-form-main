cead84e187c4a632f1b97b991b9834918d31b8a9
Darypooh29 <dchavira290@gmail.com>
2025-01-30 16:12:37 -0600

    Missing to add errors, completed form and fix radio buttons styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
-'use strict';
+"use strict";
 
-const input_name = document.querySelector('#name');
-const lastname_input = document.querySelector('#lastname');
-const email_input = document.querySelector('#email');
-const radio_button_container = document.querySelectorAll('.field-container__query-type-container');
+const input_name = document.querySelector("#name");
+const lastname_input = document.querySelector("#lastname");
+const email_input = document.querySelector("#email");
+const radio_button_container = document.querySelectorAll(
+  ".field-container__query-type-container"
+);
 
-radio_button_container.forEach(container => {
-  container.addEventListener('click', () => {
+radio_button_container.forEach((container) => {
+  container.addEventListener("click", () => {
     const radioButton = container.querySelector('input[type="radio"]');
-    
+
     radioButton.checked = true;
 
+    radio_button_container.forEach((c) => {
+      c.classList.remove("selected");
+      const input = c.querySelector('input[type="radio"]');
+      if (input) {
+        input.classList.remove("selected-input");
+      }
+    });
+
+    container.classList.add("selected");
+
+    radioButton.classList.add("selected-input");
+
     const selectedValue = radioButton.value;
-    console.log('Selected Value:', selectedValue);
+    console.log("Selected Value:", selectedValue);
   });
 });
